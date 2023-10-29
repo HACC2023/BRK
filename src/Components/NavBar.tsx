b8c242d2640c34d65eecfb6a45713590132399de
Kobe <kobeuyeda@outlook.com>
2023-10-29 12:29:48 +0000

    finished

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from "next/link";
+import Image from 'next/image';
 
 interface NavBarType {
   userId: string | null
@@ -11,14 +12,14 @@ const NavBar: React.FC<NavBarType> = ({userId}) => {
     <div className="navbar bg-primary fixed">
       <div className="flex-1">
         <a className="normal-case">
-          <img src='https://www.hpu.edu/cncs/cmdr/img/cmdr-white-lockup-logo.png' width={80} />
+          <Image alt="logo" src='https://www.hpu.edu/cncs/cmdr/img/cmdr-white-lockup-logo.png' width={80} />
         </a>
       </div>
       <div className="flex-none gap-2">
         {userId !== null ? <div className="dropdown dropdown-end">
           <label className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
-              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              <Image alt='profile' src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
             </div>
           </label>
           <ul className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
